ccbad4970ceb524d0d2eb14b0a158b1734306934
SusanLIn <linshuyan2016@gmail.com>
2025-12-08 02:47:55 +0000

    update test route

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -31,7 +31,7 @@ from .factories import RecommendationFactory
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
 )
-BASE_URL = "/recommendations"
+BASE_URL = "/api/recommendations"
 DISCOUNT_URL = f"{BASE_URL}/apply_discount"
 
 
